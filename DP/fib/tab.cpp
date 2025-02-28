ecd4f2a9689ed88ca9d6aeebf0a6aa8db6d047d6
Gaurav Singh <gauravsinghaw123@gmail.com>
2025-02-28 12:37:28 +0530

    added tabulation for fib

---
+++ DP/fib/tab.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int f(int n){
+    // dp[0]=0;
+    // dp[1]=1;
+    // for(int i=2;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+    // }
+    int prev1=0;
+    int prev2=1;
+    int ans;
+    for(int i=2;i<=n;i++){
+        ans=prev1+prev2;
+        prev2=prev1;
+        prev1=ans;
+    }
+    return ans;
+}
+int main()
+{
+cout<<"enter size";
+int n;
+cin>>n;
+cout<<f(n+1);
+return 0;
+}
